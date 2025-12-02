18dc7dbf70f67ab17e139f76b570aae148de49be
YuhaoSu <YuhaoSu@users.noreply.github.com>
2025-12-02 19:27:44 +0000

    Deploying to gh-pages from @ YuhaoSu/yuhaosu.github.io@7aaf568941d6967ea6140d3cd35b3efc0025a647 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "YuhaoSu/yuhao-su.github.io",
+    "data-repo": "YuhaoSu/yuhaosu.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
